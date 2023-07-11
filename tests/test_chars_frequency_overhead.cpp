a1068a8a0a67d3e14afb6918b5bd3a68c94977cf
CoffeeStraw <clarantonio98@gmail.com>
2023-07-11 19:22:26 +0200

    Added overhead computation and renamed chars_frequency_speed filename

---
+++ tests/test_chars_frequency_overhead.cpp
@@ -0,0 +1,101 @@
+/**
+ * Compute the overhead of the chars_frequency function.
+ */
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <ff/ff.hpp>
+#include <ff/parallel_for.hpp>
+
+#include "../src/include/io_file.hpp"
+#include "../src/include/chars_frequency.hpp"
+#include "../src/include/utimer.hpp"
+
+using namespace std;
+
+vector<int> computeMultiThreadedMeasureOverhead(const string &text, const int nWorkers)
+{
+    // Fill with vector of 256 elements initialized to 0, since this will not be done later
+    vector<vector<int>> chunksResult(nWorkers);
+    for (int i = 0; i < nWorkers; i++)
+        chunksResult[i] = vector<int>(256, 0);
+
+    // Static load balancing: each thread gets a chunk of the text of the same size
+    vector<thread> threads;
+    int chunkSize = text.length() / nWorkers;
+    int start = 0;
+    int end = chunkSize;
+
+    // Counting
+    for (int i = 0; i < nWorkers; i++)
+    {
+        if (i == nWorkers - 1)
+            end = text.length();
+
+        threads.push_back(thread(
+            [&chunksResult, i, &text, start, end]()
+            {
+                // No body, as this is no overhead
+            }));
+
+        start = end;
+        end += chunkSize;
+    }
+
+    // Merging
+    vector<int> result = vector<int>(256, 0);
+    for (int i = 0; i < nWorkers; i++)
+    {
+        threads[i].join();
+        for (int j = 0; j < 256; j++)
+            result[j] += chunksResult[i][j];
+    }
+
+    return result;
+}
+
+int main(int argc, char *argv[])
+{
+    // Read command line arguments
+    if (argc != 4)
+    {
+        cout << "Usage: " << argv[0] << " <filename> <nWorkers> <nIterations)" << endl;
+        return 1;
+    }
+    string filename = argv[1];
+    int nWorkers = atoi(argv[2]);
+    int nIterations = atoi(argv[3]);
+
+    string text = io_file::readSeq("tests/inputs/" + filename);
+
+    vector<int> charsFrequency;
+    
+    // Multi-threaded execution
+    long us, averageUs = 0;
+    for (int i = 0; i < nIterations; i++)
+    {
+        {
+            utimer t("", &us);
+            charsFrequency = chars_frequency::computeMultiThreaded(text, nWorkers);
+        }
+        averageUs += us;
+    }
+    averageUs /= nIterations;
+    cout << "Average multi-threaded execution time: " << averageUs << " us" << endl;
+
+    // Multi-threaded execution (measure overhead)
+    us, averageUs = 0;
+    for (int i = 0; i < nIterations; i++)
+    {
+        {
+            utimer t("", &us);
+            charsFrequency = computeMultiThreadedMeasureOverhead(text, nWorkers);
+        }
+        averageUs += us;
+    }
+    averageUs /= nIterations;
+    cout << "Average multi-threaded overhead time " << averageUs << " us" << endl;
+
+    return 0;
+}
---
+++ tests/test_huffman_encode_overhead.cpp
@@ -0,0 +1,212 @@
+/**
+ * Test the multi-threaded version of the Huffman encoding algorithm.
+ */
+
+#include <iostream>
+#include <thread>
+
+#include "../src/include/io_file.hpp"
+#include "../src/include/chars_frequency.hpp"
+#include "../src/include/huffman_tree.hpp"
+#include "../src/include/huffman_encode.hpp"
+#include "../src/include/utimer.hpp"
+
+using namespace std;
+
+string fromStringToBinaryMultiThreadedMeasureOverhead(const string &text, const vector<string> &huffmanMap, const int nWorkers, long &totalUs)
+{
+    string result = "";
+    vector<string> chunksResult(nWorkers);
+
+    // Compute in advance chunksResult, as it is not overhead
+    int chunkSize = text.length() / nWorkers;
+    int start = 0;
+    int end = chunkSize;
+
+    for (int i = 0; i < nWorkers; i++)
+    {
+        if (i == nWorkers - 1)
+            end = text.length();
+
+        chunksResult[i] = huffman_encode::fromStringToBinarySeq(text, start, end, huffmanMap);
+
+        start = end;
+        end += chunkSize;
+    }
+
+    // Overhead computation
+    long us;
+
+    // Compute overhead for creating threads
+    vector<thread> threads;
+    us = 0;
+    {
+        utimer t("Overhead threads", &us);
+
+        chunkSize = text.length() / nWorkers;
+        start = 0;
+        end = chunkSize;
+
+        for (int i = 0; i < nWorkers; i++)
+        {
+            if (i == nWorkers - 1)
+                end = text.length();
+
+            threads.push_back(thread([]() {}));
+            start = end;
+            end += chunkSize;
+        }
+    }
+    totalUs += us;
+
+    // Measure overhead for merging
+    us = 0;
+    {
+        utimer t("Overhead merging", &us);
+        // Merge
+        for (int i = 0; i < nWorkers; i++)
+        {
+            threads[i].join();
+            result += chunksResult[i];
+        }
+    }
+    totalUs += us;
+
+    return result;
+}
+
+string fromBinaryToASCIIMultiThreadedMeasureOverhead(const string &text, const int nWorkers, long &totalUs)
+{
+    string result = "";
+    vector<string> chunksResult(nWorkers);
+
+    // Compute in advance chunksResult, as it is not overhead
+    int chunkSize = text.length() / nWorkers;
+    int start = 0;
+    int end = chunkSize;
+
+    for (int i = 0; i < nWorkers; i++)
+    {
+        if (i == nWorkers - 1)
+            end = text.length();
+
+        chunksResult[i] = huffman_encode::fromBinaryToASCIISeq(text, start, end);
+
+        start = end;
+        end += chunkSize;
+    }
+
+    // Overhead computation
+    long us;
+
+    // Compute overhead for creating threads
+    vector<thread> threads;
+    us = 0;
+    {
+        utimer t("Overhead threads", &us);
+
+        chunkSize = text.length() / nWorkers;
+        start = 0;
+        end = chunkSize;
+
+        for (int i = 0; i < nWorkers; i++)
+        {
+            if (i == nWorkers - 1)
+                end = text.length();
+
+            threads.push_back(thread([]() {}));
+            start = end;
+            end += chunkSize;
+        }
+    }
+    totalUs += us;
+
+    // Measure overhead for merging
+    us = 0;
+    {
+        utimer t("Overhead merging", &us);
+        // Merge
+        for (int i = 0; i < nWorkers; i++)
+        {
+            threads[i].join();
+            result += chunksResult[i];
+        }
+    }
+    totalUs += us;
+
+    return result;
+}
+
+int main(int argc, char *argv[])
+{
+    // Read command line arguments
+    if (argc != 4)
+    {
+        cout << "Usage: " << argv[0] << " <filename> <nWorkers> <nIterations)" << endl;
+        return 1;
+    }
+    string filename = argv[1];
+    int nWorkers = atoi(argv[2]);
+    int nIterations = atoi(argv[3]);
+
+    string text = io_file::readSeq("tests/inputs/" + filename);
+
+    vector<int> charsFrequency = chars_frequency::computeMultiThreaded(text, nWorkers);
+    huffman_tree::Node *root = huffman_tree::buildHuffmanTree(charsFrequency);
+    vector<string> huffmanMap = huffman_tree::buildHuffmanMap(root);
+
+    // Multi-threaded execution
+    string binaryString;
+    long us, averageUs = 0;
+    for (int i = 0; i < nIterations; i++)
+    {
+        {
+            utimer t("", &us);
+            binaryString = huffman_encode::fromStringToBinaryMultiThreaded(text, huffmanMap, nWorkers);
+        }
+        averageUs += us;
+    }
+    averageUs /= nIterations;
+    cout << "Average StringToBinary multi-threaded execution time: " << averageUs << " us" << endl;
+
+    // Multi-threaded execution (measure overhead)
+    us, averageUs = 0;
+    for (int i = 0; i < nIterations; i++)
+    {
+        us = 0;
+        binaryString = fromStringToBinaryMultiThreadedMeasureOverhead(text, huffmanMap, nWorkers, us);
+        averageUs += us;
+    }
+    averageUs /= nIterations;
+    cout << "Average StringToBinary multi-threaded execution time (measure overhead): " << averageUs << " us" << endl;
+
+    // Pad one time only
+    huffman_encode::padString(binaryString);
+
+    // Multi-threaded execution
+    string asciiString;
+    us, averageUs = 0;
+    for (int i = 0; i < nIterations; i++)
+    {
+        {
+            utimer t("", &us);
+            asciiString = huffman_encode::fromBinaryToASCIIMultiThreaded(binaryString, nWorkers);
+        }
+        averageUs += us;
+    }
+    averageUs /= nIterations;
+    cout << "Average BinaryToASCII multi-threaded execution time: " << averageUs << " us" << endl;
+
+    // Multi-threaded execution (measure overhead)
+    us, averageUs = 0;
+    for (int i = 0; i < nIterations; i++)
+    {
+        us = 0;
+        asciiString = fromBinaryToASCIIMultiThreadedMeasureOverhead(binaryString, nWorkers, us);
+        averageUs += us;
+    }
+    averageUs /= nIterations;
+    cout << "Average BinaryToASCII multi-threaded execution time (measure overhead): " << averageUs << " us" << endl;
+
+    return 0;
+}
